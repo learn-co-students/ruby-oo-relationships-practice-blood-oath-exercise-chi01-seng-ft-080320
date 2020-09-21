ea7cae725bceb4067d395fd80fd5006189826bd5
Percy-gif <percymajor@gmail.com>
2020-09-21 01:31:44 -0500

    Done.

--- app/models/bloodoath.rb
+++ app/models/bloodoath.rb
@@ -7,17 +7,19 @@
         @@all = []
 
         def initialize (date, cult, follower)
-
-            @date = date
+            @date_of_initiation = date  #returns a String that is the initiation date of this blood oath in the format YYYY-MM-DD.
             @cult = cult 
             @follower = follower 
 
             @all << self
-
         end
                 
-         def self.all
-            @all           #returns an Array of all the blood oaths
-        end
-        
+        def self.all
+            @all   
+        end #returns an Array of all the blood oaths
+
+        def self.first_oath
+            self.all.first.follower 
+        end #returns the Follower instance for the follower that made the very first blood oath
+
     end 
--- app/models/follower.rb
+++ app/models/follower.rb
@@ -3,40 +3,46 @@ class Follower
 
     attr_reader :name, :age, :life_motto
 
-   @@all_cults= []
-   @@all_followers = []
+   @@all = [] #  Array of the follower's cults
 
    def initialize (name, age, life_motto)
        @name = name
        @age = age
        @life_motto = life_motto
-       @@all << self
+       
+       @@all << self #returns an Array of this follower's cults
    end
 
-   def name 
-       puts {#name}
+    def cults 
+        BloodOath.all.select{|bloodoath|bloodoath.follower == self} #capitalize bloodoat? 
+    end #returns an Array of this follower's cults
+
+    
+   def join_cult(date, cult) #is cult required as an argument??? 
+       BloodOath.new(date, cult, self)
+   end #takes in an argument of a Cult instance and adds this follower to the cult's list of followers
+
+   def self.all 
+       @@all 
    end 
 
-   def age 
-       @age
-   end 
+   def self.of_a_certain_age(age)
+       BloodOath.all.select{|bloodoath|BloodOath.follower.age >= age}
+   end #takes an Integer argument that is an age and returns an Array of followers who are the given age or older
 
-   def life_motto 
-       @life_motto
-   end 
+#    def self.of_certain_age(age)
+#        all.select do |follower|        #change the enumerable format to this one's 
+#        follower.age >= age
+#    end
 
-   def cults 
-       # returns an `Array` of this follower's cults
+   def my_cults_slogans
+   end #prints out all of the slogans for this follower's cults
 
-   end 
+   def self.most_active
+   end #returns the Follower instance who has joined the most cults
 
-   def join_cult(Cult)
-       @@all_followers << self 
-   end 
+   def self.top_ten
+       Follower.all.sort_by{|follower|follower.cults_following}.reverse.first(10)
+   end #returns an Array of followers; they are the ten most active followers
 
-   
 
-   def self.of_certain_age(age)
-       all.select do |follower|
-       follower.age >= age
-   end
